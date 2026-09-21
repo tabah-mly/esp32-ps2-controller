0496fa59c09a017abbf7f4ec5154e30a8be15830
tabah-mly <tabahmulya917@gmail.com>
2026-09-21 19:22:33 +0800

    refactor: organize frame debug

--- src/receiver.cpp
+++ src/receiver.cpp
@@ -2,6 +2,10 @@
 #include <WiFi.h>
 #include <esp_now.h>
 
+#define LED_PIN 4
+
+#define FRAME_DEBUG false
+
 #define BTN_UP (1 << 0)
 #define BTN_RIGHT (1 << 1)
 #define BTN_DOWN (1 << 2)
@@ -32,60 +36,100 @@ struct ControllerFrame {
   uint16_t buttons;
 };
 
+ControllerFrame receivedFrame = {};
+volatile bool newData = false;
+
 void onDataRecv(const uint8_t* mac, const uint8_t* data, int len) {
   if (len != sizeof(ControllerFrame)) {
-    Serial.print("Invalid packet size: ");
-    Serial.println(len);
     return;
   }
+  memcpy(&receivedFrame, data, sizeof(ControllerFrame));
+  newData = true;
+}
 
-  ControllerFrame frame;
+void actions() {
+  bool upPressed = (receivedFrame.buttons & BTN_UP) != 0;
+  bool rightPressed = (receivedFrame.buttons & BTN_RIGHT) != 0;
+  bool downPressed = (receivedFrame.buttons & BTN_DOWN) != 0;
+  bool leftPressed = (receivedFrame.buttons & BTN_LEFT) != 0;
 
-  memcpy(&frame, data, sizeof(frame));
+  bool trianglePressed = (receivedFrame.buttons & BTN_TRIANGLE) != 0;
+  bool circlePressed = (receivedFrame.buttons & BTN_CIRCLE) != 0;
+  bool crossPressed = (receivedFrame.buttons & BTN_CROSS) != 0;
+  bool squarePressed = (receivedFrame.buttons & BTN_SQUARE) != 0;
 
-  Serial.printf(
-    "UP(%d), RIGHT(%d), DOWN(%d), LEFT(%d), TRIANGLE(%d), CIRCLE(%d), CROSS(%d), SQUARE(%d), L1(%d), L2(%d), L3(%d), R1(%d), R2(%d), R3(%d), SELECT(%d), START(%d), LX(%d), LY(%d), RX(%d), RY(%d)\n",
+  bool L1Pressed = (receivedFrame.buttons & BTN_L1) != 0;
+  bool L2Pressed = (receivedFrame.buttons & BTN_L2) != 0;
+  bool L3Pressed = (receivedFrame.buttons & BTN_L3) != 0;
 
-    (frame.buttons & BTN_UP) != 0,
-    (frame.buttons & BTN_RIGHT) != 0,
-    (frame.buttons & BTN_DOWN) != 0,
-    (frame.buttons & BTN_LEFT) != 0,
+  bool R1Pressed = (receivedFrame.buttons & BTN_R1) != 0;
+  bool R2Pressed = (receivedFrame.buttons & BTN_R2) != 0;
+  bool R3Pressed = (receivedFrame.buttons & BTN_R3) != 0;
 
-    (frame.buttons & BTN_TRIANGLE) != 0,
-    (frame.buttons & BTN_CIRCLE) != 0,
-    (frame.buttons & BTN_CROSS) != 0,
-    (frame.buttons & BTN_SQUARE) != 0,
+  bool selectPressed = (receivedFrame.buttons & BTN_SELECT) != 0;
+  bool startPressed = (receivedFrame.buttons & BTN_START) != 0;
 
-    (frame.buttons & BTN_L1) != 0,
-    (frame.buttons & BTN_L2) != 0,
-    (frame.buttons & BTN_L3) != 0,
+  int8_t lxDirection = receivedFrame.lx;
+  int8_t lyDirection = receivedFrame.ly;
+  int8_t rxDirection = receivedFrame.rx;
+  int8_t ryDirection = receivedFrame.ry;
 
-    (frame.buttons & BTN_R1) != 0,
-    (frame.buttons & BTN_R2) != 0,
-    (frame.buttons & BTN_R3) != 0,
+  if (FRAME_DEBUG) {
+    Serial.printf(
+      "UP(%d), RIGHT(%d), DOWN(%d), LEFT(%d), "
+      "TRIANGLE(%d), CIRCLE(%d), CROSS(%d), SQUARE(%d), "
+      "L1(%d), L2(%d), L3(%d), "
+      "R1(%d), R2(%d), R3(%d), "
+      "SELECT(%d), START(%d), "
+      "LX(%d), LY(%d), RX(%d), RY(%d)\n",
 
-    (frame.buttons & BTN_SELECT) != 0,
-    (frame.buttons & BTN_START) != 0,
+      upPressed,
+      rightPressed,
+      downPressed,
+      leftPressed,
 
-    frame.lx,
-    frame.ly,
-    frame.rx,
-    frame.ry);
+      trianglePressed,
+      circlePressed,
+      crossPressed,
+      squarePressed,
 
-  Serial.println();
+      L1Pressed,
+      L2Pressed,
+      L3Pressed,
+
+      R1Pressed,
+      R2Pressed,
+      R3Pressed,
+
+      selectPressed,
+      startPressed,
+
+      lxDirection,
+      lyDirection,
+      rxDirection,
+      ryDirection);
+  }
+
+  Serial.println(L2Pressed);
+  if (L2Pressed) {
+    digitalWrite(LED_PIN, HIGH);
+  } else {
+    digitalWrite(LED_PIN, LOW);
+  }
 }
 
 void setup() {
   Serial.begin(115200);
 
-  delay(1000);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
 
-  Serial.println();
-  Serial.println("ESP-NOW Receiver");
-  Serial.println("================");
+  delay(1000);
 
   WiFi.mode(WIFI_STA);
 
+  Serial.println();
+  Serial.println("Receiver");
   Serial.print("Receiver MAC: ");
   Serial.println(WiFi.macAddress());
 
@@ -100,5 +144,9 @@ void setup() {
 }
 
 void loop() {
-  delay(10);
+  if (newData) {
+    newData = false;
+    actions();
+    delay(1);
+  }
 }
